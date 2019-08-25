7d7136bef47cf5836abd84d9ba88df3e7a39971a
rjonathans <rjonath@live.com>
2019-08-25 17:29:58 -0300

    Implementação lógica dos metodos: registrar curso e excluir curso na classe DigitalHouseManager.

--- src/br/com/digitalhouse/DigitalHouseManager.java
+++ src/br/com/digitalhouse/DigitalHouseManager.java
@@ -2,6 +2,7 @@ package br.com.digitalhouse;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class DigitalHouseManager {
 
@@ -14,18 +15,38 @@ public class DigitalHouseManager {
 
     //Methods
     public void registrarCurso(String novoNome, Integer novoCodigoCurso,Integer novaQuantidadeMaximaDeAlunos){
-        Curso novoCurso = new Curso(novoNome,novoCodigoCurso,novaQuantidadeMaximaDeAlunos);
+        //cria e instancia um novo urso com todos os atributos que o construtor pede
+        Curso novoCurso = new Curso(novoNome,novoCodigoCurso,novaQuantidadeMaximaDeAlunos);     //********cria um curso
+        //aqui adicinamos o novo curso instanciado na linha anterior dentro do list curso
         curso.add(novoCurso);
-        System.out.println("Curso de "+novoCurso.getNomeCurso()+" regstrado com sucesso!");
+        //imprime informando no console do sucesso do procedimento
+        System.out.println("Curso de "+novoCurso.getNomeCurso()+" registrado com sucesso!");
     }
 
-    public void excluirCurso(Integer codigoCurso){
+    public void excluirCurso(Integer codigoCurso) {
+        //Crio uma variável local sem atrbuir um valor
+        Curso cursoEncontrado ;
+        //verifica um por um todos os objetos do list curso usando a variável local cursoProcurado
+        for (Curso cursoProcurado : curso) {
+            //se encontrar um objeto com o mesmo código
+            if (cursoProcurado.getCodigoCurso().equals(codigoCurso)) {
+                //ele atribui à variavel curso encontrada, o valor do objeto onde ele esta contido
+                cursoEncontrado = cursoProcurado;
+                //usando o valor atribuido a variavel local curso encontrado, agora podemos remover o objeto com o codigo equivalente ao solicitado
+                curso.remove(cursoEncontrado);
+                //em seguido imprime na tela avisando que o procedimento de remoção foi bem sucedido
+                System.out.println("Curso removido com sucesso.");
+            }
+            //se nao encontrar apenas imprime na tela que não foi possivel
+            System.out.println("Curso não cadastrado!");
+        }
+    }
+
+    public void registrarProfessorAdjunto(String nome, String sobrenome, Integer codigoProfessor, Integer quantidadeDeHoras) {
 
     }
-    public void registrarProfessorAdjunto(String nome, String sobrenome, Integer codigoProfessor, Integer quantidadeDeHoras){
 
-    }
-    public void registrarProfessorTitular(String nome, String sobrenome, Integer codigoProfessor, String especialidade){
+    public void registrarProfessorTitular(String nome, String sobrenome, Integer codigoProfessor, String especialidade) {
 
     }
     public void excluirProfessor(Integer codigoProfessor){
